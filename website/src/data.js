55863021cc875bc2665a0c684e727880caef555b
PIYUSH MANI CHAUBEY <42182675+MANIPIYUSH@users.noreply.github.com>
2023-01-24 21:26:08 +0530

    portfolio

---
+++ website/src/data.js
@@ -0,0 +1,60 @@
+export const projects = [
+    {
+      title: "Movie Recomendation App",
+      subtitle: "React-js",
+      description:
+        "In this Website i have add the features of showing set of trending movies with their Genre Name on Home Page ",
+      image: "./project-1.gif",
+      link: "https://imdb-movie-application.netlify.app/",
+    },
+    {
+      title: "QR-Code Generator",
+      subtitle: "HTML CSS JAVASCRIPT",
+      description:
+        "In This app we have to paste any link and the app will generate the QR code of that link ",
+      image: "./project-2.gif",
+      link: "https://qr-codegenerator-app.netlify.app/",
+    },
+    {
+      title: "Background Color Changer",
+      subtitle: "HTML CSS JAVASCRIPT",
+      description:
+               " This is the background-color change app by one click we get the random background color ",
+      image: "./project-3.gif",
+      link: "https://randombackgroundchanger.netlify.app/",
+    },
+    {
+      title: "Password Generator",
+      subtitle: "HTML CSS JAVASCRIPT",
+      description:"This is the password generator app we can generate the different set of random password",
+      image: "./project-4.gif",
+      link: "https://passwordgeneratorrandomly.netlify.app/",
+    },
+  ];
+  
+  export const testimonials = [
+    {
+      quote:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+      image: "https://randomuser.me/api/portraits/men/1.jpg",
+      name: "Jesse Hicks",
+      company: "Zoozle",
+    },
+    {
+      quote:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+      image: "https://randomuser.me/api/portraits/men/94.jpg",
+      name: "Ruben Alvarez",
+      company: "Dooble.io",
+    },
+  ];
+  
+  export const skills = [
+    "JavaScript",
+    "React-js",
+    "Next.js",
+    "Tailwind/Bootstrap",
+    "Material UI",
+    "Problem-Solving",
+  ];
+  
